be618aad0e34ea45c7f8ed4ed7f98e42c9aee20b
logonpf <logonpf@fiap.com.br>
2018-03-19 23:07:13 -0300

    Initial commit

---
+++ app/src/main/java/br/com/catacoquinho/fakezap/firebase/fcm/MeuFirebaseInstanceIdService.kt
@@ -0,0 +1,15 @@
+package br.com.catacoquinho.fakezap.firebase.fcm
+
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+import com.orhanobut.hawk.Hawk
+
+class MeuFirebaseInstanceIdService : FirebaseInstanceIdService() {
+
+
+    override fun onTokenRefresh() {
+        super.onTokenRefresh()
+        Hawk.put("TOKENFIREBASE", FirebaseInstanceId.getInstance().token)
+    }
+
+}
